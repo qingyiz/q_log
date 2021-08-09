32472a548e0e27598cda5a99ecd4cfb36d0d4104
qingyiz <qingyizhu12138@163.com>
2021-08-09 16:10:52 +0800

    添加宏定义，宏定义可以输出到屏幕和文件

--- lo_logs.hpp
+++ lo_logs.hpp
@@ -22,13 +22,36 @@
 #define SERVERLOG_SKIP				" -> "
 #define SERVERLOG_ENTER				"\r\n"
 
+#define COLOR_NONE          "\033[0m"
+#define COLOR_BLACK         "\033[0;30m"
+#define COLOR_BLUE          "\033[0;34m"
+#define COLOR_GREEN         "\033[0;32m"
+#define COLOR_CYAN          "\033[0;36m"
+#define COLOR_RED           "\033[0;31m"
+#define COLOR_YELLOW        "\033[1;33m"
+#define COLOR_WHITE         "\033[1;37m"
+
 #define LOGTYPE_WARNING			0
 #define LOGTYPE_INFO			1
 #define LOGTYPE_DEBUG			2
 #define LOGTYPE_FATAL			3
 #define LOGTYPE_TRACE			4
 
-
+#define Q_WAR(fmt, args...) ({printf(COLOR_YELLOW"[WAR]:%s[%d]: "COLOR_NONE, __FUNCTION__,__LINE__);printf(fmt, ##args);\
+								LoLogs::WriteLog(LOGTYPE_WARNING,fmt,##args);\
+								})
+#define Q_INF(fmt, args...) ({printf(COLOR_BLUE"[INF]:%s[%d]: "COLOR_NONE, __FUNCTION__,__LINE__);printf(fmt, ##args);\
+								LoLogs::WriteLog(LOGTYPE_INFO,fmt,##args);\
+								})
+#define Q_DEB(fmt, args...) ({printf(COLOR_GREEN"[DEB]:%s[%d]: "COLOR_NONE, __FUNCTION__,__LINE__);printf(fmt, ##args);\
+								LoLogs::WriteLog(LOGTYPE_DEBUG,fmt,##args);\
+								})
+#define Q_FAT(fmt, args...) ({printf(COLOR_RED"[FAT]:%s[%d]: "COLOR_NONE, __FUNCTION__,__LINE__);printf(fmt, ##args);\
+								LoLogs::WriteLog(LOGTYPE_FATAL,fmt,##args);\
+								})
+#define Q_TRA(fmt, args...) ({printf(COLOR_CYAN"[TRA]:%s[%d]: "COLOR_NONE, __FUNCTION__,__LINE__);printf(fmt, ##args);\
+								LoLogs::WriteLog(LOGTYPE_TRACE,fmt,##args);\
+								})
 class LoLogs
 {
 public:
